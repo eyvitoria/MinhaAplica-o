56fd27f625e1780667a9b6cc2701f7f868259baf
Emilly Vitoria <vitoriamatias152@gmail.com>
2020-05-02 23:39:01 -0300

    Parte 1

---
+++ src/factory/ConnectionFactory.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factory;
+/* situa em qual package ou "pacote" está a classe 
+package br.com.projeto.factory;*/
+
+// faz as importações de classes necessárias para o funcionamento do programa 
+import java.sql.Connection; 
+// conexão SQL para Java 
+import java.sql.DriverManager; 
+// driver de conexão SQL para Java 
+import java.sql.SQLException; 
+// classe para tratamento de exceções 
+/**
+ *
+ * @author Emilly Vitória
+ */
+public class ConnectionFactory {
+
+
+     public Connection getConnection() {
+		 try {
+			return DriverManager.getConnection("jdbc:mysql://localhost/projetojava", "root", "");
+		 }         
+		 catch(SQLException excecao) {
+			throw new RuntimeException(excecao);
+		 }
+     }
+
+
+}
